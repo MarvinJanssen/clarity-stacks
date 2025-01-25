616b12d08ba4695dd666c33a3146615d6a271b23
MarvinJanssen <MarvinJanssen@users.noreply.github.com>
2025-01-25 23:56:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,125 @@
+# clarity-stacks
+
+A Clarity library to check if a specific transaction ID was mined in a Stacks block.
+
+Inspired by the excellent [clarity-bitcoin](https://github.com/friedger/clarity-bitcoin) library.
+
+With thanks to @Friedger.
+
+## Why?
+
+Why not? In all seriousness, this library allows you to check if a specific transaction has been mined in a Stacks block in Clarity. It is particularly useful to prove if a given contract has a specific code body. Since there is no Clarity function to fetch a transaction nor to read a contract code body, this is the next best thing.
+
+The inability to deploy a contract from an existing contract is sometimes a limiting factor for protocols. Whilst this library does not provide such a feature, it does allow a protocol to accept a contract deployed by a third-party by verifying if the code body is as it expects.
+
+## Clarity functions
+
+### clarity-stacks
+
+Stateless contract that verifies if a specific transaction ID was mined in a block.
+
+Use `was-tx-mined-compact`, it returns `(ok true)` if the transaction was mined. It takes the following parameters:
+
+- `txid`: 32-byte txid buffer
+- `proof`: tuple with a merkle proof (see below)
+- `tx-block-height`: block height of the transaction to verify
+- `block-header-without-signer-signatures`: as described (see below)
+
+### code-body-prover
+
+Stateless contract that verifies if a contract with a specific code body was mined in a block.
+
+Use `is-contract-deployed`, it returns `(ok true)` if the transaction was mined. It takes the following parameters:
+
+- `nonce`: tx nonce as an 8-byte buffer
+- `fee`: tx fee as an 8-byte buffer
+- `signature`: 65-byte signature
+- `contract`: contract principal to check
+- `code-body`: code body to prove
+- `proof`: tuple with a merkle proof (see below)
+- `tx-block-height`: block height of the transaction to verify
+- `block-header-without-signer-signatures`: as described (see below)
+
+### clarity-stacks-helper
+
+Contains helper functions to calculate block hashes that check the length of the inputs:
+
+- `block-header-hash`: calculates the block header hash.
+- `block-id-header-hash`: calculates the block ID.
+
+## Merkle proof
+
+The merkle proof is a tuple in the form of:
+
+```clarity
+{ tx-index: uint, hashes: (list 14 (buff 32)), tree-depth: uint}
+```
+
+There is a helper function in `clarity-stacks.ts` to generate it for you. (See below.)
+
+## Block header without signer signatures
+
+Required to be able to calculate the block header hash. It should be a buffer containing the entire block header of a Nakamoto block with the signer signatures stripped out. It should look like this:
+
+- version: 1 byte
+- chain_length: 8 bytes
+- burn_spent: 8 bytes
+- consensus_hash: 20 bytes
+- parent_block_id: 32 bytes
+- tx_merkle_root: 32 bytes
+- state_index_root: 32 bytes
+- timestamp: 8 bytes
+- miner_signature: 65 bytes
+- signer_bitvec: 2 bytes bitvec bit count + 4 bytes buffer length + bitvec buffer
+
+Luckily for you, there is a helper function in `clarity-stacks.ts` to generate it for you. (See below.)
+
+## clarity-stacks.ts
+
+A minimal TypeScript library to make using the clarity-stacks library a bit easier.
+
+### Class MerkleTree
+
+Feed it a list of transaction IDs and it will construct a Stacks-compatible merkle tree for you.
+
+```ts
+const tree = MerkleTree::new([txid1, txid2, txid3, txid4]);
+const root = tree.root(); // get the merkle root
+const proof = tree.proof(0); // generate a proof for txid1
+```
+
+### Creating proof tuples
+
+Construct a merkle tree as above, then use the helper function:
+
+```ts
+const proof = tree.proof(0);
+const proofCV = proof_path_to_cv(0, proof, 2); // tx_index, proof, tree depth
+```
+
+### Fetching and preparing block headers
+
+There are helper functions to fetch, parse, and prepare blocks. Uses the new v3 API that returns raw blocks. 
+
+```ts
+const block = await fetch_nakamoto_block_struct(block_height);
+const block_header_without_signer_signatures = raw_block_header(block);
+```
+
+### Example script
+
+A small example file at `src/example.ts` that uses some of the TypeScript library functions. It can be executed directly with `npx ts-node src/example.ts`.
+
+## Unit tests
+
+There are some, mainly to illustrate how to use the Clarity and TypeScript library. Run `npm test` after installing dependencies.
+
+## Future developments
+
+The library requires information that the Stacks chain already knows to be submitted, like the block header, transaction IDs, and so on. It makes the library wasteful in that sense. A Clarity function that fetches a code body or returns a hash of a code body would make the library redundant. If you like this library, then consider voicing your support for such a feature.
+
+```clarity
+(get-code-body 'SP123....my-contract)
+(get-code-body-sha256 'SP123....my-contract)
+```
+
---
+++ src/clarity-stacks.ts
@@ -0,0 +1,211 @@
+import { sha512_256 } from '@noble/hashes/sha512';
+import { bytesToHex, hexToBytes } from "@noble/hashes/utils";
+import { bufferCV, BytesReader, deserializeTransaction, listCV, tupleCV, uintCV } from '@stacks/transactions';
+import type { StacksTransaction, TupleCV } from '@stacks/transactions';
+
+export const NAKAMOTO_SIGNER_SIGNATURE_LENGTH = 65;
+
+export function tagged_sha512_256(tag: Uint8Array, data: Uint8Array): Uint8Array {
+    return sha512_256(new Uint8Array([...tag, ...data]));
+}
+
+export type NakamotoBlockStruct = {
+    version: Uint8Array, // 1 byte
+    chain_length: Uint8Array, // 8 bytes
+    burn_spent: Uint8Array, // 8 bytes
+    consensus_hash: Uint8Array, // 20 bytes
+    parent_block_id: Uint8Array, // 32 bytes
+    tx_merkle_root: Uint8Array, // 32 bytes
+    state_index_root: Uint8Array, // 32 bytes
+    timestamp: Uint8Array, // 8 bytes
+    miner_signature: Uint8Array, // 65 bytes
+    signer_signatures: Uint8Array[], // 65 bytes per signature
+    signer_bitvec: Uint8Array, // 2 bytes bitvec bit count + 4 bytes buffer length + bitvec buffer
+    transactions: StacksTransaction[]
+}
+
+// Adopted from https://github.com/stacks-network/stacks-core/blob/eb865279406d0700474748dc77df100cba6fa98e/stacks-common/src/util/hash.rs
+export class MerkleTree {
+    static MERKLE_PATH_LEAF_TAG = new Uint8Array([0x00]);
+    static MERKLE_PATH_NODE_TAG = new Uint8Array([0x01]);
+
+    nodes: Uint8Array[][];
+
+    constructor(nodes: Uint8Array[][] = []) {
+        this.nodes = nodes;
+    }
+
+    static empty(): MerkleTree {
+        return new MerkleTree();
+    }
+
+    static new(data: Uint8Array[]): MerkleTree {
+        if (data.length === 0) {
+            return new MerkleTree();
+        }
+
+        let leaf_hashes: Uint8Array[] = data.map(buf => MerkleTree.get_leaf_hash(buf));
+
+        // force even number
+        if (leaf_hashes.length % 2 !== 0) {
+            const dup = leaf_hashes[leaf_hashes.length - 1];
+            leaf_hashes.push(dup);
+        }
+
+        let nodes: Uint8Array[][] = [leaf_hashes];
+
+        while (true) {
+            const current_level = nodes[nodes.length - 1];
+            const next_level: Uint8Array[] = [];
+
+            for (let i = 0; i < current_level.length; i += 2) {
+                if (i + 1 < current_level.length) {
+                    next_level.push(MerkleTree.get_node_hash(current_level[i], current_level[i + 1]));
+                } else {
+                    next_level.push(current_level[i]);
+                }
+            }
+
+            // at root
+            if (next_level.length === 1) {
+                nodes.push(next_level);
+                break;
+            }
+
+            // force even number
+            if (next_level.length % 2 !== 0) {
+                const dup = next_level[next_level.length - 1];
+                next_level.push(dup);
+            }
+
+            nodes.push(next_level);
+        }
+
+        return new MerkleTree(nodes);
+    }
+
+    static get_leaf_hash(leaf_data: Uint8Array): Uint8Array {
+        return tagged_sha512_256(MerkleTree.MERKLE_PATH_LEAF_TAG, leaf_data);
+    }
+
+    static get_node_hash(left: Uint8Array, right: Uint8Array): Uint8Array {
+        return tagged_sha512_256(MerkleTree.MERKLE_PATH_NODE_TAG, new Uint8Array([...left, ...right]));
+    }
+
+    proof(index: number): Uint8Array[] {
+        if (this.nodes.length === 0) {
+            return [];
+        }
+        if (index > this.nodes[0].length-1) {
+            throw new Error("Index out of bounds");
+        }
+        const depth = this.nodes.length - 1;
+        const path = Math.pow(2, depth) + index;
+
+        let proof: Uint8Array[] = [];
+        let position = index;
+        for (let level = 0 ; level < depth ; ++level) {
+            const left = ((1 << level) & path) > 0;
+            proof.push(this.nodes[level][position + (left ? -1 : 1)]);
+            position = ~~(position / 2);
+        }
+
+        return proof;
+    }
+
+    root(): Uint8Array {
+        if (this.nodes.length === 0) {
+            return new Uint8Array(32);
+        }
+        return this.nodes[this.nodes.length - 1][0];
+    }
+
+    pretty_print(): string {
+        let str = '';
+        for (let level = this.nodes.length-1 ; level >= 0 ; --level) {
+            const whitespace = " ".repeat((this.nodes.length-level-1)*2);
+            str += this.nodes[level].map(node => whitespace+bytesToHex(node)+"\n").join('');
+        }
+        return str;
+    }
+}
+
+export function merkle_tree_from_txs(txs: StacksTransaction[]): MerkleTree {
+    return MerkleTree.new(txs.map(tx => hexToBytes(tx.txid())));
+}
+
+export function block_header_hash(block: NakamotoBlockStruct): Uint8Array {
+    return sha512_256(raw_block_header(block));
+}
+
+export function block_index_header_hash(block_sighash: Uint8Array, consensus_hash: Uint8Array): Uint8Array {
+    return sha512_256(new Uint8Array([...block_sighash, ...consensus_hash]));
+}
+
+export function raw_block_header(block: NakamotoBlockStruct): Uint8Array {
+    return new Uint8Array([
+        ...block.version,
+        ...block.chain_length,
+        ...block.burn_spent,
+        ...block.consensus_hash,
+        ...block.parent_block_id,
+        ...block.tx_merkle_root,
+        ...block.state_index_root,
+        ...block.timestamp,
+        ...block.miner_signature,
+        ...block.signer_bitvec
+    ]);
+}
+
+export function proof_path_to_cv(tx_index: number, hashes: Uint8Array[], tree_depth: number): TupleCV {
+    return tupleCV({
+        "tx-index": uintCV(tx_index),
+        "hashes": listCV(hashes.map(bufferCV)),
+        "tree-depth": uintCV(tree_depth)
+    });
+}
+
+export async function fetch_raw_nakamoto_block(height: number, block_api?: string): Promise<Uint8Array> {
+    block_api = block_api || 'https://api.hiro.so/v3/blocks/height/';
+    const response = await fetch(`${block_api}${height}`);
+    return await (response as any).bytes();
+}
+
+export function parse_raw_nakamoto_block(raw_block: Uint8Array): NakamotoBlockStruct {
+    const reader = new BytesReader(raw_block);
+    let block: NakamotoBlockStruct = {
+        version: reader.readBytes(1),
+        chain_length: reader.readBytes(8),
+        burn_spent: reader.readBytes(8),
+        consensus_hash: reader.readBytes(20),
+        parent_block_id: reader.readBytes(32),
+        tx_merkle_root: reader.readBytes(32),
+        state_index_root: reader.readBytes(32),
+        timestamp: reader.readBytes(8),
+        miner_signature: reader.readBytes(65),
+        signer_signatures: [],
+        signer_bitvec: new Uint8Array(),
+        transactions: []
+    };
+    const signature_count = reader.readUInt32BE();
+    for (let i = 0; i < signature_count; i++) {
+        block.signer_signatures.push(reader.readBytes(NAKAMOTO_SIGNER_SIGNATURE_LENGTH));
+    }
+    reader.consumed += 2; // skip signer bitvec bit count
+    const bitvec_byte_length = reader.readUInt32BE();
+    reader.consumed -= 6; // go back to the start of the bitvec
+    block.signer_bitvec = reader.readBytes(2 + 4 + bitvec_byte_length);
+    const tx_count = reader.readUInt32BE();
+    for (let i = 0 ; i < tx_count ; ++i) {
+        block.transactions.push(deserializeTransaction(reader));
+    }
+    if (reader.consumed !== raw_block.length) {
+        throw new Error("Left-over bytes in raw block");
+    }
+    return block;
+}
+
+export async function fetch_nakamoto_block_struct(height: number, block_api?: string): Promise<NakamotoBlockStruct> {
+    const raw_block = await fetch_raw_nakamoto_block(height, block_api);
+    return parse_raw_nakamoto_block(raw_block);
+}
